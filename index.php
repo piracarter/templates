f61094e147965a56f60326c4d681c5b744ed2d27
Jesus Arriola <piracarter@hotmail.com>
2018-07-05 18:49:05 -0500

    se agregan flags

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
 echo '¡Mi nombre de usuario es ' . $_ENV["NOMBRE_USUARIO"] . '! Mi estado de animo: ' . $_ENV["ESTADO_ANIMO"];
 
+$flag_uptime = $_ENV["FLAG_UPTIME"];
+$flag_uname = $_ENV["FLAG_UNAME"];
 
-$salida = shell_exec('uptime');
-echo "<P>$salida</P>";
+if ( strcmp($flag_uptime,"yes") == 0 ) {
+  $salida = shell_exec('uptime');
+  echo "<P>$salida</P>";
+}
 
-$salida = shell_exec('uname -a');
-echo "<P>$salida</P>";
-
+if ( strcmp($flag_uname,"yes") == 0 ) {
+  $salida = shell_exec('uname -a');
+  echo "<P>$salida</P>";
+}
 ?>
