6c84bdead2f14163a475c5fdc237753bbbd9e29a
Jesus Arriola <piracarter@hotmail.com>
2018-07-05 17:27:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 echo '¡Mi nombre de usuario es ' . $_ENV["NOMBRE_USUARIO"] . '! Mi estado de animo: ' . $_ENV["ESTADO_ANIMO"];
+
+
+$salida = shell_exec('uptime');
+echo "<pre>$salida</pre>";
 ?>
